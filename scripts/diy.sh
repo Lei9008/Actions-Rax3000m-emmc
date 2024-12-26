3bec3ffcff316cb80e7c32b62573e7e4f7b3917e
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2024-12-27 07:59:40 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -9,3 +9,34 @@ sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7
 #修改主机名
 sed -i "s/hostname='OpenWrt'/hostname='RAX3000Z'/g" package/base-files/files/bin/config_generate
 
+
+
+
+
+# 应用过滤
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
+# 京东签到
+#git clone https://github.com/noiver/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+
+# 商店
+git clone https://github.com/linkease/istore-ui package/istore-ui
+git clone https://github.com/linkease/istore package/istore
+
+# 网易云解灰（天灵）
+#git clone https://github.com/UnblockNeteaseMusic/server package/server
+
+# 简单MESH
+git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
+
+# 集客AC控制器
+git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
+
+# AdguardHome-app
+git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+
+# MosDns
+#git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+
+# alist文件列表
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
