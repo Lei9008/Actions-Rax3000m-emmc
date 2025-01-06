042b8a37855697f5b891e7f891fac6a484098907
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2025-01-06 08:12:54 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 
-
-#修改默认IP
+# 修改默认IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
-#web登陆密码从空密码修改为 password
+# web登陆密码从空密码修改为 password
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
-#修改主机名
-#sed -i "s/hostname='OpenWrt'/hostname='Rax3000z'/g" package/base-files/files/bin/config_generate
+# 修改主机名
+sed -i "s/hostname='OpenWrt'/hostname='Rax3000z'/g" package/base-files/files/bin/config_generate
 
 
 ##-----------------Del duplicate packages------------------
