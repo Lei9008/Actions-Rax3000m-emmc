cd5634cb168e248862267ebbbdde576a6b707ca3
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2025-01-10 18:38:21 +0800

    Update 1diy.sh

--- scripts/1diy.sh
+++ scripts/1diy.sh
@@ -15,11 +15,17 @@
 #修改默认IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
-#web登陆密码从空密码修改为 password
-sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
-#修改主机名
-sed -i "s/hostname='OpenWrt'/hostname='Rax3000z'/g" package/base-files/files/bin/config_generate
+#web登陆密码从空密码修改为 password
+sed -i "s/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g" package/base-files/files/etc/shadow
+
+##更改主机名
+sed -i "s/hostname='.*'/hostname='RAX 3000Z'/g" package/base-files/files/bin/config_generate
+
+##New WiFi
+sed -i "s/ImmortalWrt-2.4G/HiWiFi/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/ImmortalWrt-5G/HiWiFi_5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
 
 
 ##-----------------Del duplicate packages------------------
